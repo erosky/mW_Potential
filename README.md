3848c6226562d20ac1dd09f26ed57b8ed43bfa13
Elise Rosky <emrosky@mtu.edu>
2020-06-30 13:10:04 -0400

    add ice 1h melting simulations

---
+++ README.md
@@ -0,0 +1,12 @@
+# mW_Potential
+
+Introductory LAMMPS simulations involving the mW water potential
+
+### Liquid_Box
+Periodic box containing liquid water.
+
+### Ice_Box
+Periodic box containing solid water
+
+### Ice_Seed
+Placing solid and liquid water in contact inside a periodic box.
---
+++ Reproduce_Tmelt_1h/Trial_3/Analysis_Scripts/auto_analysis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TEMPERATURES=('268K' '269K' '270K' '271K' '272K' '273K' '274K' '275K')
+WORKING_DIR=~/mW_Potential/Reproduce_Tmelt_1h/Trial_3/Analysis_Scripts
+LOG_FILE=${WORKING_DIR}/auto_analysis.log
+DATETIME=$(date +"%Y-%m-%d_%H:%M:%S")
+
+# Log date and time
+echo "${DATETIME}" >> ${LOG_FILE}
+
+# Run simulation in each temperature directory
+for temp in "${TEMPERATURES[@]}"; do
+    cd "${WORKING_DIR}/../$temp" && \
+    # Check that input file exists before running simulation
+    if [ -f log.run_Tmelt_${temp}_AUTO ]; then
+        echo "$temp input file found: Ready to analyze $temp simulation" >> ${LOG_FILE}
+        # Run script, direct errors into log file
+        ${WORKING_DIR}/auto_analysis_run.sh log.run_Tmelt_${temp}_AUTO run_Tmelt_${temp}_AUTO
+        
+    else
+        echo "Input file does not exist for $temp simulation" >> ${LOG_FILE}
+    fi
+done
+
+
